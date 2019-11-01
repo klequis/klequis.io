7187edf51efaf3406a5e31e3e47d27353824a382
klequis <klequis@gmail.com>
2019-11-01 07:51:16 -0700

    correct margins

--- site/src/components/part-of-book.js
+++ site/src/components/part-of-book.js
@@ -1,23 +1,23 @@
 import React from "react"
 import Image from "gatsby-image"
-import { rhythm } from "../utils/typography"
+import { rhythm, scale } from "../utils/typography"
 
 const blockQuoteStyle = {
+	borderLeft: '0.54375rem solid #1ca086',
+	borderLeftColor: 'inherit',
+	color: 'inherit',
+	fontSize: '1.1487rem',
+	fontStyle: 'italic',
+	lineHeight: '1.45rem',
+	marginBottom: rhythm(2),
 	marginLeft: 0,
 	marginRight: 0,
-	marginTop: 0,
+    marginTop: rhythm(3),
+	opacity: 0.8,
 	paddingBottom: 0,
 	paddingLeft: '0.90625rem',
 	paddingRight: 0,
 	paddingTop: 0,
-	marginBottom: '1.45rem',
-	fontSize: '1.1487rem',
-	lineHeight: '1.45rem',
-	borderLeft: '0.54375rem solid #1ca086',
-	color: 'inherit',
-	fontStyle: 'italic',
-	borderLeftColor: 'inherit',
-	opacity: 0.8,
 }
 
 function PartOfBook() {
