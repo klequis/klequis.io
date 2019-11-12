8900a76c530b8459613e3087c07cc2df14c4c3ed
klequis <klequis@gmail.com>
2019-11-12 11:26:35 -0800

    clean-up

--- site/src/components/part-of-book.js
+++ site/src/components/part-of-book.js
@@ -1,33 +1,24 @@
-import React from "react"
-import { rhythm } from "../utils/typography"
+import React from 'react'
 import { Link } from 'gatsby'
 import BlockQuote from './block-quote'
 
-const blockQuoteStyle = {
-	borderLeft: '0.54375rem solid #1ca086',
-	borderLeftColor: 'inherit',
-	color: 'inherit',
-	fontSize: '1.1487rem',
-	fontStyle: 'italic',
-	lineHeight: '1.45rem',
-	marginBottom: rhythm(2),
-	marginLeft: 0,
-	marginRight: 0,
-    marginTop: rhythm(3),
-	opacity: 0.8,
-	paddingBottom: 0,
-	paddingLeft: '0.90625rem',
-	paddingRight: 0,
-	paddingTop: 0,
-}
-
 function PartOfBook() {
   return (
-    <BlockQuote>
-        <p>This article is <Link to='/full-stack-mern-development-series'>part of a series of articles</Link> on full-stack MERN (MongoDB, Express, React & Node) development. For the full series see <Link to='/full-stack-mern-development-series'>Full-stack Mern Development</Link>.</p>
+    <BlockQuote marginTopRhythm={2} marginBottomRhythm={3}>
+      <p>
+        This article is{' '}
+        <Link to="/full-stack-mern-development-series">
+          part of a series of articles
+        </Link>{' '}
+        on full-stack MERN (MongoDB, Express, React & Node) development. For the
+        full series see{' '}
+        <Link to="/full-stack-mern-development-series">
+          Full-stack Mern Development
+        </Link>
+        .
+      </p>
     </BlockQuote>
   )
 }
-
 
 export default PartOfBook
